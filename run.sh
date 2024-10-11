ef44b7b6c6c5ab3fa06d4cc7e4c1e5a1ae9ef729
coldwindx <15653913658@163.com>
2024-10-11 15:58:47 +0800

    transformer

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@
 python core/eval.py --pretrain /mnt/sdd1/data/zhulin/pretrain/bert_pretrain_uncased/ \
                     --cls_model SingleChannelPredictor \
                     --cls_dataset SingleChannelDataset \
-                    --model /mnt/sdd1/data/zhulin/jack/models/epoch=29-step=2999010.ckpt \
+                    --model /mnt/sdd1/data/zhulin/jack/models/epoch=15-step=1599472.ckpt \
                     --dataset /mnt/sdd1/data/zhulin/jack/cdatasets.test.5.csv \
                     --batch_size 8 \
                     --output /mnt/sdd1/data/zhulin/jack/scores/Transformer.npy
@@ -46,7 +46,9 @@ python core/eval.py --pretrain /mnt/sdd1/data/zhulin/pretrain/bert_pretrain_unca
 #                     --csv /mnt/sdd1/data/zhulin/jack/cdatasets.train.csv
 
 # python core/data.py --task model_2_model --model /mnt/sdd1/data/zhulin/jack/models/Transformer.ckpt
-# python core/data.py --task model_2_model --model /mnt/sdd1/data/zhulin/jack/models/epoch=29-step=2999010.ckpt
+# python core/data.py --task model_2_model --model /mnt/sdd1/data/zhulin/jack/models/epoch=15-step=1599472.ckpt
+
+
 # [Train Fasttext]
 # python core/pretrain.py --task TfidfVectorizer --dataset /home/zhulin/datasets/cdatasets.train.5.csv --output tfidf.ph
 # python core/test.py --fasttext ./common/fasttext.ph \
